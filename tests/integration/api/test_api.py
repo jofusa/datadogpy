09b938718de903756c03ef79e608e8f3cc54b4fd
Charles Lai <charles.lai@datadoghq.com>
2015-07-14 17:00:42 -0400

    add to skeleton tests for embeds

--- tests/integration/api/test_api.py
+++ tests/integration/api/test_api.py
@@ -753,14 +753,22 @@ class TestDatadog(unittest.TestCase):
 
         dog.Host.unmute(hostname)
 
+    @attr('embed')
     def test_get_all_embeds(self):
         all_embeds = dog.Embed.get_all()
         print all_embeds
+        # TODO: Check all embeds
         pass
 
+    @attr('embed')
     def test_get_embed(self):
+        embed_id = "some_crazy_64_character_embed_token_here"
+        result = dog.Embed.get(embed_id)
+        print result
+        # TODO: Check result
         pass
 
+    @attr('embed')
     def test_create_embed(self):
         # Initialize a graph definition
         graph_def = {
@@ -781,17 +789,30 @@ class TestDatadog(unittest.TestCase):
                 }]
             }]
         }
+        timeframe = "1_hour"
         size = "medium"
         legend = "no"
 
         graph_json = json.dumps(graph_def)
-        print graph_json
+        result = dog.Embed.create(graph_json=graph_json, timeframe=timeframe, size=size, legend=legend)
+        print result
+        # TODO: Check result
         pass
 
+    @attr('embed')
     def test_enable_embed(self):
+        embed_id = "some_crazy_64_character_embed_token_here"
+        result = dog.Embed.enable(embed_id)
+        print result
+        # TODO: Check result
         pass
 
+    @attr('embed')
     def test_revoke_embed(self):
+        embed_id = "some_crazy_64_character_embed_token_here"
+        result = dog.Embed.revoke(embed_id)
+        print result
+        # TODO: Check result
         pass
 
 if __name__ == '__main__':
